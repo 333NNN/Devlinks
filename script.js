22528e42c7fe3e1338ce4db4a5dc67de812e037a
333NNN <nata.cunha@aluno.senai.br>
2023-12-04 09:05:33 -0300

    esclerose

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function toggleMode() {
   const html = document.documentElement;
-
   html.classList.toggle("light");
 
+  // pegar a tag img
   const img = document.querySelector("#profile img");
+
+  // substituir a imagem
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png");
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar2.jpg");
   } else {
-    img.setAttribute("src", "./assets/avatar.png");
+    // set tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar2-invert.jpg");
   }
 }
